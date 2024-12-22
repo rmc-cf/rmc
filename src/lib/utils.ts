6b36f30a13b6f94bdb998febc624d77d90872bce
rmc-cf <rmcyyds@gmail.com>
2024-12-22 15:21:32 +0800

    update path

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-export function getUrl(url:string){
-  return import.meta.env.BASE_URL+url
+export function getUrl(url: string) {
+  const baseUrl = import.meta.env.BASE_URL;
+  // 去除 baseUrl 和 url 的多余斜杠
+  const normalizedBaseUrl = baseUrl.replace(/\/$/, ''); // 去除 baseUrl 末尾的斜杠
+  const normalizedUrl = url.replace(/^\//, ''); // 去除 url 开头的斜杠
+  return `${normalizedBaseUrl}/${normalizedUrl}`;
 }
